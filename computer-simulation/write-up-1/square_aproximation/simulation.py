e79fa135bff8111722407bde23378d2e662e141e
Kacper Malachowski <kacpermaachowski@gmail.com>
2024-04-16 21:46:24 +0200

    Description of second exercise

--- computer-simulation/write-up-1/square_aproximation/simulation.py
+++ computer-simulation/write-up-1/square_aproximation/simulation.py
@@ -4,7 +4,8 @@ def monte_carlo_square_aproximation(runs, a, b):
   xs = np.random.uniform(0, a, runs)
   ys = np.random.uniform(0, b, runs)
 
-  inFigure = ((xs <= 2) & (ys <= xs)) | (((xs > 2) & (xs <= 6)) & (ys <= -(xs/4)+2.5))  | ((xs > 6) & (ys <= -(xs/2)+4))
+  inFigure = ((xs <= 2) & (ys <= xs)) | (((xs > 2) & (xs <= 6)) 
+    & (ys <= -(xs/4)+2.5))  | ((xs > 6) & (ys <= -(xs/2)+4))
 
   mcArea = (sum(inFigure) / runs) * (a*b)
 
