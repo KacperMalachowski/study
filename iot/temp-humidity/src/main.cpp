dc9653f12c89f8c41f658855780415334fcc35cd
Kacper Malachowski <kacpermaachowski@gmail.com>
2024-12-04 23:57:37 +0100

    Fix

--- iot/temp-humidity/src/main.cpp
+++ iot/temp-humidity/src/main.cpp
@@ -1,149 +1,69 @@
 #include <string>
 #include <random>
 #include <Arduino.h>
-#include <PicoMQTT.h>
+#include <PubSubClient.h>
+#include <ESP8266WiFi.h>
 #include <DHT.h>
-#include <ArduinoJson.h>
 #include "secrets.h"
 
 #define DHTPIN 0
-#define MQTT_BROKER "mqtt.eclipseprojects.io"
+#define MQTT_BROKER "167.235.62.0"
+#define DEVICE_ID "Temp_Humidity_04474"
+#define MQTT_PREF "tir_us/" + string(DEVICE_ID)
 
 using namespace std;
 
-
-PicoMQTT::Client mqtt("mqtt.eclipseprojects.io");
+WiFiClient espClient;
+PubSubClient client(espClient);
 DHT dht;
-String uuid = String(random(0xffff), HEX);
-int readingPeriod;
 
-String prepareDiscoveryMsg() {
-  JsonDocument res;
-  res["idx"] = uuid;
-  res["mqtt"] = "tir_us/" + uuid;
-
-  JsonObject tempSensor = res["sensors"].add<JsonObject>();
-  tempSensor["name"] = "Temperature";
-  tempSensor["mqtt"] = "temperature";
-
-  JsonObject humSensor = res["sensors"].add<JsonObject>();
-  humSensor["name"] = "Humidity";
-  humSensor["mqtt"] = "humidity";
-
-  JsonObject setReadingPeriod = res["commands"].add<JsonObject>();
-  setReadingPeriod["name"] = "setReadingPeriod";
-  setReadingPeriod["type"] = "integer";
-  setReadingPeriod["min"] = dht.getMinimumSamplingPeriod();
-
-  String out;
-  res.shrinkToFit();
-
-  serializeJson(res, out);
-  return out;
-}
-
-void setup() {
-  // Setup sensors
-  dht.setup(DHTPIN, DHT::DHT11);
-  readingPeriod = dht.getMinimumSamplingPeriod();
-
-  // Setup serial
-  Serial.begin(115200);
-
-  // Setup WiFi
+void setup_wifi() {
   WiFi.begin(SECRET_SSID, SECRET_PASS);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.println("Connecting to WiFi...");
+    Serial.print(".");
+  }
+  Serial.println("WiFi connected");
+  Serial.printf("IP address: %s\n", WiFi.localIP().toString().c_str());
+}
 
-    if (WiFi.status() == WL_CONNECT_FAILED) {
-      WiFi.begin(SECRET_FALLBACK_SSID, SECRET_FALLBACK_PASS);
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect("Temp_Humidity", MQTT_USER, MQTT_PASS)) {
+      Serial.println("connected");
+    } else {
+      Serial.printf("failed, rc=%d try again in 5 seconds\n", client.state());
+      delay(5000);
     }
   }
-  Serial.println("Connected to WiFi: " + WiFi.SSID() + " with IP: " + WiFi.localIP().toString());
+}
 
-  // Setup MQTT
-  mqtt.subscribe("tir_us/discovery", [](const char * topic, const char * payload) {
-    // Check if the payload contains discovery command in format {"command": "discover"}
-    JsonDocument req;
-    DeserializationError error = deserializeJson(req, payload);
-    if (error) {
-      String err = String(error.c_str());
-      Serial.println("Failed to parse discovery request" + err);
-      mqtt.publish("tir_us/" + uuid, "{\"error\": \"Failed to parse discovery request "+ err +"\"}");
-      return;
-    }
-  
-    if (req["command"] == "discover") {
-      String out = prepareDiscoveryMsg();
-      
-      mqtt.publish("tir_us/discovery", out);
-    }
-  });
+void setup() {
+  Serial.begin(9600);
+  setup_wifi();
+  client.setServer(MQTT_BROKER, 1883);
 
-  mqtt.subscribe("tir_us/" + uuid, [](const char * topic, const char * payload) {
-    JsonDocument req;
-    DeserializationError error = deserializeJson(req, payload);
-    if (error) {
-      String err = String(error.c_str());
-      Serial.println("Failed to parse command request" + err);
-      mqtt.publish(topic, "{\"error\": \"Failed to parse command request "+ err +"\"}");
-      return;
-    }
-
-    if (req["command"] == "setReadingPeriod") {
-      int newReadingPeriod = req["value"];
-      if (newReadingPeriod < dht.getMinimumSamplingPeriod()) {
-        mqtt.publish(topic, "{\"error\": \"Reading period cannot be less than " + String(dht.getMinimumSamplingPeriod()) + "\"}");
-        return;
-      }
-
-      readingPeriod = newReadingPeriod;
-      mqtt.publish(topic, "{\"status\": \"Reading period set to " + String(readingPeriod) + "\", \"value\": " + String(readingPeriod) + "}");
-    }
-
-    if (req["command"] == "discover") {
-      String out = prepareDiscoveryMsg();
-      
-      mqtt.publish(topic, out);
-    }
-  });
-
-  mqtt.begin();
+  dht.setup(DHTPIN, DHT::DHT11);
 }
 
 void loop() {
-  mqtt.loop();
-
-  float temperature = dht.getTemperature();
-  float humidity = dht.getHumidity();
-
-  DHT::DHT_ERROR_t status = dht.getStatus();
-  if (status != DHT::ERROR_NONE) {
-    Serial.println("Failed to read from DHT sensor, status: " + status);
-    delay(dht.getMinimumSamplingPeriod());
-    return;
+  char msg[8];
+  if (!client.connected()) {
+    reconnect();
   }
 
+  float humidity = dht.getHumidity();
+  float temperature = dht.getTemperature();
 
-  Serial.println("Temperature: " + String(temperature) + "°C, Humidity: " + String(humidity) + "%");
+  snprintf(msg, 8, "%f", humidity);
+  client.publish((MQTT_PREF + "/humidity").c_str(), msg);
+  snprintf(msg, 8, "%f", temperature);
+  client.publish((MQTT_PREF + "/temperature").c_str(), msg);
 
-  JsonDocument tempDoc;
-  tempDoc["temperature"] = temperature;
-  tempDoc["unit"] = "C";
+  Serial.printf("Humidity: %f\n", humidity);
+  Serial.printf("Temperature: %f\n", temperature);
 
-  JsonDocument humDoc;
-  humDoc["humidity"] = humidity;
-  humDoc["unit"] = "%";
-
-  char tempBuffer[1024];
-  char humBuffer[1024];
-
-  serializeJson(tempDoc, tempBuffer);
-  serializeJson(humDoc, humBuffer);
-
-  mqtt.publish("tir_us/" + uuid + "/temperature", tempBuffer);
-  mqtt.publish("tir_us/" + uuid + "/humidity", humBuffer);
-
-  delay(readingPeriod);
+  delay(dht.getMinimumSamplingPeriod());
 }
+
